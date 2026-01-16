658df5fe47e32744fe574fd6a3f441228d7549f3
goldenvertex42 <fabled93@gmail.com>
2026-01-16 13:47:27 -0600

    chore: remove unused imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import '../styles/App.css'
 import Header from './Header'
 import CardGrid from './CardGrid'
