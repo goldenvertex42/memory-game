7fedd7c5d0607d507237e446871dc1469b274f86
goldenvertex42 <fabled93@gmail.com>
2026-01-16 12:54:33 -0600

    feat: create CardGrid component that creates a card for each object in cards array passed from App.jsx

---
+++ src/components/CardGrid.jsx
@@ -0,0 +1,18 @@
+import Card from "./Card";
+import shuffleArray from "../utils/shuffleArray";
+
+export default function CardGrid({cards, handleClick}) {
+    return (
+    <div className="card-grid">
+      {cards.map((card) => (
+        <Card key={card.id} 
+              card={card}
+              name={card.name} 
+              pokemonId={card.pokemonId}
+              handleClick={handleClick}  
+               />
+      ))}
+    </div>
+  );
+}
+
