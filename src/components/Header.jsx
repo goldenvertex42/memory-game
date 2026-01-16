13db05efdc555ac70312151466ecb71cc0ab9eed
goldenvertex42 <fabled93@gmail.com>
2026-01-16 13:49:39 -0600

    feat: add instuctions to header and style them

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 export default function Header() {
     return(
-        <h1>Pokémon Memory Game</h1>
+        <div className="header">
+            <h1>Pokémon Memory Game</h1>
+            <div className="instructions-container">
+                <h2 className="instructions-heading"><span className="underlined">Instructions</span></h2>
+                <p className="instructions">Each click the game shuffles and remembers who you clicked on; try not to click the same pokémon twice!</p>
+            </div>
+        </div>
     );
 }
