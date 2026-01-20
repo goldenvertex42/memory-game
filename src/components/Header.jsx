95256da6db87e35c943ed72362c83b5d2311aead
goldenvertex42 <fabled93@gmail.com>
2026-01-19 23:00:23 -0600

    chore: edit title of game

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
     return(
         <div className="header">
-            <h1>Pokémon Memory Game</h1>
+            <h1>Lavender Town Memory Game</h1>
             <div className="instructions-container">
                 <h2 className="instructions-heading"><span className="underlined">Instructions</span></h2>
-                <p className="instructions">Each click the game shuffles and remembers who you clicked on; try not to click the same pokémon twice!</p>
+                <p className="instructions">Each click the game shuffles and remembers who you clicked on; try not to click the same Pokémon twice!</p>
             </div>
         </div>
     );
