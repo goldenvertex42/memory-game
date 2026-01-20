70f7c0c3f6bf9de12efeb0d2ac4ca68349d3bbeb
goldenvertex42 <fabled93@gmail.com>
2026-01-19 23:00:03 -0600

    chore: edit database for lavender town theme and import audio player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,24 +4,25 @@ import Header from './Header'
 import CardGrid from './CardGrid'
 import Scoreboard from './Scoreboard'
 import shuffleArray from '../utils/shuffleArray'
+import AudioPlayer from './MusicButton'
+import lavenderTheme from '../assets/lavender-town-theme-alt.mp3'
 
 const cards = [
-  {name: 'Mew', id: 0, pokemonId: 151},
-  {name: 'Goomy', id: 1, pokemonId: 704},
-  {name: 'Trubbish', id: 2, pokemonId: 568},
-  {name: 'Rhyhorn', id: 3, pokemonId: 111},
-  {name: 'Noivern', id: 4, pokemonId: 715},
-  {name: 'Sylveon', id: 5, pokemonId: 700},
-  {name: 'Dragonite', id: 6, pokemonId: 149},
+  {name: 'Marowak', id: 0, pokemonId: 105},
+  {name: 'Gastly', id: 1, pokemonId: 92},
+  {name: 'Haunter', id: 2, pokemonId: 93},
+  {name: 'Gengar', id: 3, pokemonId: 94},
+  {name: 'Sinistea', id: 4, pokemonId: 854},
+  {name: 'Misdreavus', id: 5, pokemonId: 200},
+  {name: 'Shuppet', id: 6, pokemonId: 353},
   {name: 'Mimikyu', id: 7, pokemonId: 778},
-  {name: 'Cyndaquil', id: 8, pokemonId: 155},
-  {name: 'Smoliv', id: 9, pokemonId: 928},
+  {name: 'Duskull', id: 8, pokemonId: 355},
+  {name: 'Litwick', id: 9, pokemonId: 607},
   {name: 'Greavard', id: 10, pokemonId: 971},
-  {name: 'Sprigatito', id: 11, pokemonId: 906},
-  {name: 'Scolipede', id: 12, pokemonId: 545},
-  {name: 'Wooper', id: 13, pokemonId: 194},
-  {name: 'Chimchar', id: 14, pokemonId: 390},
-  {name: 'Jigglypuff', id: 15, pokemonId: 39},
+  {name: 'Honedge', id: 11, pokemonId: 679},
+  {name: 'Phantump', id: 12, pokemonId: 708},
+  {name: 'Dreepy', id: 14, pokemonId: 885},
+  {name: 'Drifloon', id: 15, pokemonId: 425},
 ]
 
 function App() {
@@ -61,6 +62,7 @@ function App() {
   return (
     <>
       <Header />
+      <AudioPlayer src={lavenderTheme}/>
       <CardGrid cards={cardsArray}
                 handleClick={handleClick} />
       <Scoreboard score={score}
